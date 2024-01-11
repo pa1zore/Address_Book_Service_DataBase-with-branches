23f8c48f8d509491a0e56537645db8df1908cfed
pa1zore <pavanzore345@gmail.com>
2024-01-11 08:34:54 +0530

    Added: inserted new  contact to address book

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -25,5 +25,13 @@ phone_number varchar(20) primary key,
 email varchar(255)
 );
 
+/*
+  UC3 ability to insert new contact in the address book database
+  system
+
+*/
+insert into contact_table(first_name,last_name,address,city,state,zip,phone_number,email) values
+('david','warner','n5s3,sojanaager,kalegaon','mumbai','maharashtra','345543','000000000','warner@gmail.com'),
+('michle','hustey','ere,sdf,kalegaon','pune','maharashtra','333333','1111111111','michel@gmail.com');
 
 
